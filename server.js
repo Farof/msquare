64932d3c2eac98dbb3baf07ea8ee0184af85dc8f
Farof <gfarof@gmail.com>
2012-04-19 00:14:18 +0200

    basic start & stop handling and logging

--- server.js
+++ server.js
@@ -2,6 +2,44 @@
 
 const http = require('http');
 const frisbee = require('./frisbee');
+var closeMsg = 'stop server';
+
+function log(msg) {
+  console.log(['[' + new Date() + ']', msg].join(' '));
+}
+
+function sigQuit(msg) {
+  closeMsg = msg;
+  if (server && !closing) {
+    server.close();
+  }
+  process.exit(0);
+}
+
+process.on('exit', function () {
+  log(closeMsg);
+});
+
+// -2 & ^C
+process.on('SIGINT', function () {
+  process.stdout.write('\n');
+  sigQuit('server interupted');
+});
+
+// -9
+process.on('SIGKILL', function () {
+  sigQuit('server killed');
+});
+
+// -3
+process.on('SIGQUIT', function () {
+  sigQuit('server quitted');
+});
+
+// 15
+process.on('SIGTERM', function () {
+  sigQuit('server terminated');
+});
 
 const app = frisbee()
               .use(frisbee.static({
@@ -12,4 +50,9 @@ const app = frisbee()
 
 var server = http.createServer(app);
 
+server.on('close', function () {
+  process.exit(0);
+});
+
+log('start server');
 server.listen(80);
